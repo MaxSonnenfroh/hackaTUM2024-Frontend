5480340fa1a0d109df75283570df2546f754ca05
Max Sonnenfroh <max.sonnenfroh@freenet.de>
2024-11-23 12:09:12 +0100

    add map component and enhance card/home components with dynamic data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CardComponent } from '../card/card.component';
+import { CommonModule } from '@angular/common';
+import { WebSocketService } from '../services/websocket.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  messages: string[] = [];
+
+  constructor(private wsService: WebSocketService) {
+    this.wsService.getMessages().subscribe((message: string) => {
+      console.log('Message received:');
+      this.messages.push(message);
+      console.log(this.messages);
+    });
+  }
+
+  cards: CardComponent[] = [{
+    title: 'Total Time',
+    value: 1
+  }, {
+    title: 'Average Wait Time',
+    value: 2
+  },
+  {
+    title: 'Average Load',
+    value: 3
+  },
+  {
+    title: 'Vehicles with load > 75%',
+    value: 4
+  },
+  {
+    title: 'Vehicles with load < 25%',
+    value: 5
+  },
+  {
+    title: 'Customers with wait time > 10 mins',
+    value: 6
+  }
+]
 
 }
--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -8,7 +8,7 @@ export class WebSocketService {
   private socket$: WebSocketSubject<any>;
 
   constructor() {
-    this.socket$ = webSocket('ws://localhost:8000/ws'); // Update URL as per your backend
+    this.socket$ = webSocket('ws://127.0.0.1:8000/ws');// Update URL as per your backend
   }
 
   sendMessage(message: any) {
@@ -16,6 +16,7 @@ export class WebSocketService {
   }
 
   getMessages() {
+    console.log('getMessages triggered');
     return this.socket$; // Returns an observable for incoming messages
   }
 }
