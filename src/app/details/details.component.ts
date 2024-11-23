53548f5a694d6780de64fd2d1a6bc62f76286860
Max Sonnenfroh <max.sonnenfroh@freenet.de>
2024-11-23 22:51:30 +0100

    Refactor DetailsComponent layout and styles; implement grid structure for vehicle details and enhance data display

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { UpdateMessage } from '../types/UpdateMessage';
 
 @Component({
   selector: 'app-manage',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css'
 })
-export class ManageComponent {
+export class ManageComponent implements OnInit {
+  top3Vehicles: {id: string, percentage: number}[] = [];
+
+  message!: UpdateMessage;
+
+  totalTime: string = "";
+  averageWait: number = 0;
+  averageUtilization: number = 0;
+  loadBigger75: {id: string, percentage: number}[] = [];
+  loadSmaler25: {id: string, percentage: number}[] = [];
+  extremeWaitTime: {id: string, time: string}[] = [];
+  waitingCustomers: string[] = [];
+  customersOnTransit: string[] = [];
+  dropedCustomers: string[] = [];
+  currentDistance: {[key: string]: number} = {};
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.top3Vehicles = this.loadBigger75.sort((a, b) => b.percentage - a.percentage);
+    this.top3Vehicles = this.top3Vehicles.slice(0, 3);
+  }
 
 }
